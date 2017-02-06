925c01475e6f202a4b709279c737c53ca2d69519
Rosie <r.gollancz@gmail.com>
2017-02-06 14:40:55 +0000

    Add tests for FAQ click-through buttons

--- spec/features/visit_faq_spec.rb
+++ spec/features/visit_faq_spec.rb
@@ -13,9 +13,34 @@ RSpec.feature "Navigating to FAQs", :type => :feature do
 end
 
 RSpec.feature "The FAQ page", :type => :feature do
+  it "allows you to click through to the Appointments page" do
+    visit_faq_page
+    first(:xpath, "//a[@href='/what-we-do/appointments/']").click
+    expect(page).to have_content 'Video & phone GP appointments'
+  end
   it "allows you to click through to the Prescriptions page" do
     visit_faq_page
     first(:xpath, "//a[@href='/what-we-do/prescriptions/']").click
     expect(page).to have_content 'Speedy prescription service'
+  end
+  it "allows you to click through to the Our Doctors page" do
+    visit_faq_page
+    first(:xpath, "//a[@href='/about-us/our-doctors/']").click
+    expect(page).to have_content 'The best healthcare experts'
+  end
+  it "allows you to click through to the Patient Records page" do
+    visit_faq_page
+    first(:xpath, "//a[@href='/what-we-do/patient-records/']").click
+    expect(page).to have_content 'Your medical history'
+  end
+  it "allows you to click through to the Pricing page" do
+    visit_faq_page
+    first(:xpath, "//a[@href='/pricing/']").click
+    expect(page).to have_content 'The right plan for you and your family'
+  end
+  it "allows you to click through to the Travelling Abroad page" do
+    visit_faq_page
+    first(:xpath, "//a[@href='/what-we-do/travelling-abroad/']").click
+    expect(page).to have_content 'Speak to doctors you know and trust'
   end
 end
